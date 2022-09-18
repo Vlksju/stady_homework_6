638d9476750d1df64dec20c8ff9aac4fe45adc67
Vlksju <xenya.vol2017@yandex.ru>
2022-09-18 17:24:54 +0500

    Solved task 41

---
+++ task_41/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Программа котрая подсчитывает кличество отрицательных элементов, введенных с клавиатуры");
+Console.WriteLine("Введите элементы массива через пробел");
+
+int [] numbers = Console.ReadLine().Split().Select( x=> Convert.ToInt32(x)).ToArray();
+
+int schet = 0;
+
+for (int i=0; i<= numbers.Length-1; i++)
+{
+    if (numbers[i]<0) {schet++;}
+}
+
+Console.WriteLine($"Количество отрицательных элементов = {schet}");
